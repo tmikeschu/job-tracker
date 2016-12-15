1e51ecb625effacbec5a7cbe3c64ab4f446cbfaa
Mike Schutte <tmikeschutte@gmail.com>
2016-12-15 15:15:36 -0700

    Update factories with Faker gem

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,10 +3,33 @@ require 'factory_girl_rails'
 FactoryGirl.define do
 
   factory :job do
-    sequence :title do |n|
-      "Job#{n * (rand(3)+1)}"
+    sequence :title do 
+      Faker::Name.title
     end
 
+    sequence :description do
+      Faker::Lorem.sentence
+    end
+
+    sequence :level_of_interest do |n|
+      rand(99) + 1
+    end
+
+    sequence :city do
+      Faker::Address.city
+    end
+  end
+
+  factory :company do
+    sequence :name do |n|
+      Faker::Company.name
+    end
+
+    factory :company_with_jobs do
+      after(:create) do |company|
+        company.jobs = create_list(:job, 10)
+      end
+    end
   end
 
 end
