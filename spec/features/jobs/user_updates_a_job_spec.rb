102daa54f51bbf0af988c31d6e7dae00fd4436e6
Mike Schutte <tmikeschutte@gmail.com>
2016-12-16 06:55:04 -0700

    Remove user story comment for update job

--- spec/features/jobs/user_updates_a_job_spec.rb
+++ spec/features/jobs/user_updates_a_job_spec.rb
@@ -1,15 +1,3 @@
-=begin 
-As a user
-assuming there is a company and jobs in the database
-if I visit a company page
-and visit an individual job page by clicking its title
-and click "Edit"
-and update the job's info
-and click "Update"
-I will be back on the show page
-and the displayed information will be updated
-=end
-
 require 'rails_helper'
 
 RSpec.feature 'User updates a job' do
@@ -18,13 +6,13 @@ RSpec.feature 'User updates a job' do
     @company = create(:company_with_jobs)
     @jobs    = @company.jobs
     @job     = @jobs[5]
+    visit company_jobs_path(@company)
+    click_on @job.title
+    click_on "Edit"
   end
 
   scenario "they update information about a job" do
     new_title = Faker::Name.title
-    visit company_jobs_path(@company)
-    click_on @job.title
-    click_on "Edit"
     fill_in "job_title", with: new_title
     fill_in "job_description", with: Faker::Lorem.sentence
     fill_in "job_level_of_interest", with: (rand(99) + 1)
@@ -37,9 +25,6 @@ RSpec.feature 'User updates a job' do
 
   context "they input invalid data" do
     scenario "and they see an error" do
-      visit company_jobs_path(@company)
-      click_on @job.title
-      click_on "Edit"
       fill_in "job_title", with: ""
       fill_in "job_level_of_interest", with: ""
       fill_in "job_city", with: ""
