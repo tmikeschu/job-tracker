c16dad68ad86b89e1b24c6a046595a28fc0f7749
Mike Schutte <tmikeschutte@gmail.com>
2016-12-16 07:43:58 -0700

    Refactor see all companies feature test

--- spec/features/companies/user_sees_all_companies_spec.rb
+++ spec/features/companies/user_sees_all_companies_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-describe "User sees all companies" do
+RSpec.feature "User sees all companies" do
   scenario "a user sees all the companies" do
-    company = Company.create!(name: "ESPN")
+    company     = Company.create!(name: "ESPN")
     company_two = Company.create!(name: "Disney")
 
     visit companies_path
 
-    expect(page).to have_content("ESPN")
+    expect(page).to have_link "ESPN", href: company_path(company)
+    expect(page).to have_link "Disney", href: company_path(company_two)
   end
-
 end
