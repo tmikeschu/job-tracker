1f3504334c0e62f80ad3102ee227cd85bfaf1a2a
Mike Schutte <tmikeschutte@gmail.com>
2016-12-16 06:51:15 -0700

    Refactor feature test for show job

--- spec/features/jobs/user_sees_a_job_spec.rb
+++ spec/features/jobs/user_sees_a_job_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
-describe "User sees a specific job" do
+RSpec.feature "User sees a specific job" do
   scenario "a user sees a job for a specific company" do
-    company = Company.create!(name: "ESPN")
-    job = company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
+    company = create(:company_with_jobs)
+    job = company.jobs.first
 
     visit company_job_path(company, job)
 
-    expect(page).to have_content("ESPN")
-    expect(page).to have_content("Developer")
-    expect(page).to have_content("70")
+    expect(page).to have_content job.company.name
+    expect(page).to have_content job.title
+    expect(page).to have_content job.description
+    expect(page).to have_content job.level_of_interest
+    expect(page).to have_content job.city
   end
 end
