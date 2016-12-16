e58143af76202fb8644bd9f6453f92c05060b0d0
Mike Schutte <tmikeschutte@gmail.com>
2016-12-16 07:47:12 -0700

    Refactor show feature test for companies

--- spec/features/companies/user_sees_one_company_spec.rb
+++ spec/features/companies/user_sees_one_company_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe "User sees one company" do
+RSpec.feature "User sees one company" do
   scenario "a user sees a company" do
     company = Company.create!(name: "ESPN")
     company.jobs.create!(title: "Developer", level_of_interest: 90, city: "Denver")
 
     visit company_path(company)
 
-    expect(current_path).to eq("/companies/#{company.id}/jobs")
+    expect(current_path).to eq company_jobs_path(company)
     expect(page).to have_content("ESPN")
-    expect(page).to have_content("Developer")
+    expect(page).to have_link "Developer", href: company_job_path(company, company.jobs.first)
   end
 end
