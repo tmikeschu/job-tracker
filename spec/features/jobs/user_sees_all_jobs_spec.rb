1fcffdc95546230168ee18ca1804cee577c68e45
Mike Schutte <tmikeschutte@gmail.com>
2016-12-16 06:51:28 -0700

    Refactor jobs index feature test

--- spec/features/jobs/user_sees_all_jobs_spec.rb
+++ spec/features/jobs/user_sees_all_jobs_spec.rb
@@ -1,15 +1,13 @@
 require 'rails_helper'
 
-describe "User sees all jobs" do
+RSpec.feature "User sees all jobs" do
   scenario "a user sees all the jobs for a specific company" do
-    company = Company.create!(name: "ESPN")
-    company.jobs.create!(title: "Developer", level_of_interest: 70, city: "Denver")
-    company.jobs.create!(title: "QA Analyst", level_of_interest: 70, city: "New York City")
+    company = create(:company_with_jobs)
 
     visit company_path(company)
 
-    expect(page).to have_content("ESPN")
-    expect(page).to have_content("Developer")
-    expect(page).to have_content("QA Analyst")
+    company.jobs.each do |job|
+      expect(page).to have_link job.title, company_job_path(company, job)
+    end
   end
 end
