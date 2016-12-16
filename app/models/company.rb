e86111e54d9a93293ecd3c67eac08873622e65da
Mike Schutte <tmikeschutte@gmail.com>
2016-12-16 07:13:06 -0700

    Refactor company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,6 @@
 class Company < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
-  has_many :jobs, dependent: :destroy
+  
+  has_many  :jobs, dependent: :destroy
 end
