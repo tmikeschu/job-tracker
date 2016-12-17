340bcb7bcaaac1ab3f7b42cee3e277764f5261a7
Mike Schutte <tmikeschutte@gmail.com>
2016-12-16 21:47:48 -0700

    Sanitize categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
+  
   before_action :set_category, only: [:show, :edit, :destroy, :update]
+  
   def index
     @categories = Category.all
   end
