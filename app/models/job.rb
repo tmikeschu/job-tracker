3d85fa38083048c34aa0a6ca9f474c2107b410e2
Mike Schutte <tmikeschutte@gmail.com>
2016-12-18 14:47:01 -0700

    Sanitize models

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,8 +1,8 @@
 class Job < ActiveRecord::Base
-  validates :title, :level_of_interest, :city, presence: true
+  validates  :title, :level_of_interest, :city, presence: true
   
   belongs_to :category
   belongs_to :company
   
-  has_many :comments
+  has_many   :comments
 end
