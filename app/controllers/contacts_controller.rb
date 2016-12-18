4c19c26a122b27c29cd6098fcbeeaea5713e53b1
Mike Schutte <tmikeschutte@gmail.com>
2016-12-18 11:11:31 -0700

    Refactor contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,18 +1,13 @@
 class ContactsController < ApplicationController
+  include ContactsHelper
+  
   before_action :set_company, only: [:create, :show]
 
   def create
     @jobs = @company.jobs
     @contact = Contact.new(contact_params)
     @contact.company_id = @company.id
-    if @contact.save
-      flash[:success] = "#{@contact.first_name + " " + @contact.last_name} added!"
-      redirect_to company_path(@company)
-    else
-      @contacts = @company.contacts
-      @errors = @contact.errors.full_messages
-      render "jobs/index"
-    end
+    save_contact(@contact)
   end
 
   def index
