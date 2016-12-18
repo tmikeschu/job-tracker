3f25d2f328eb0e013dcb2cbca6e4d685fabb2177
Mike Schutte <tmikeschutte@gmail.com>
2016-12-17 23:28:35 -0700

    Add contacts resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :companies, except: [:patch] do
     resources :jobs, except: [:patch]
+    resources :contacts, only: [:create]
   end
 
   resources :categories, except: [:patch]
