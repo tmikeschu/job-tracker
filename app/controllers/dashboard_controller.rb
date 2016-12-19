9f5bf4e36ddaec0aeef502b64693226e83f3c04e
Mike Schutte <tmikeschutte@gmail.com>
2016-12-18 22:39:04 -0700

    Refactor dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,4 @@
 class DashboardController < ApplicationController
   def show
-    @jobs = Job.all
-    @companies = Company.all
-    @interest_level_and_jobs = Job.group(:level_of_interest).order("level_of_interest DESC").count
   end
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -11,4 +11,30 @@ module DashboardHelper
     Job.group(:city).count
   end
 
+  def jobs
+    Job.all
+  end
+
+  def interest_level_and_jobs
+    Job.group(:level_of_interest).order("level_of_interest DESC").count
+  end
+
+  def interest_ranges
+    %w(0-9 10-19 20-29 30-39 40-49 50-59 60-69 70-79 80-89 90-99 100+)
+  end
+
+  def jobs_in_range
+    interest_ranges.map do |range|
+      if range.include?("-")
+        range = range.partition("-")
+        Job.where(level_of_interest: [range.first.to_i..range.last.to_i]).count
+      else
+        Job.where("level_of_interest >= ?", 100).count
+      end
+    end
+  end
+
+  def city_jobs_count
+    Job.group(:city).count
+  end
 end
