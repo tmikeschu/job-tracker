5903b52d74942acd92317724b11bf43161892f8e
Mike Schutte <tmikeschutte@gmail.com>
2016-12-18 22:10:02 -0700

    Add helper methods for index action

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -34,4 +34,39 @@ module JobsHelper
   def full_name(contact)
     "#{contact.first_name.capitalize} #{contact.last_name.capitalize}"
   end
+
+  def jobs_in_city(city)
+    Job.where(city: city)
+  end
+
+  def jobs_with_level_of_interest(level_of_interest)
+    Job.where(level_of_interest: level_of_interest)
+  end
+
+  def levels_of_interest
+    Job.pluck(:level_of_interest).sort.reverse
+  end
+
+  def cities
+    Job.pluck(:city)
+  end
+
+  def decide_which_index(params)
+    @jobs = Job.all
+    return sorted_jobs(params) if params[:sort]
+    return location_jobs(params) if params[:location]
+    @jobs  = @company.jobs
+    @contact = Contact.new
+    @contacts = @company.contacts
+  end
+
+  def sorted_jobs(params)
+    render :city_sorted     if params[:sort] == "location"
+    render :interest_sorted if params[:sort] == "interest"
+  end
+
+  def location_jobs(params)
+    @city = params[:location]
+    render :location
+  end 
 end
