85ca184cee2c36e85dc89ce84bb811b73a7223ad
Mike Schutte <tmikeschutte@gmail.com>
2016-12-18 19:57:06 -0700

    Change dashboard controller interest level var

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   def show
     @jobs = Job.all
     @companies = Company.all
-    @interest_level_and_jobs = Job.group(:level_of_interest).count
+    @interest_level_and_jobs = Job.group(:level_of_interest).order("level_of_interest DESC").count
   end
 end
