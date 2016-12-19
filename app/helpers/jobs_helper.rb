9a898bc36d1c5400f44abed66a40a59ab940f18b
Mike Schutte <tmikeschutte@gmail.com>
2016-12-18 22:39:14 -0700

    Refactor jobs controller

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,8 +1,7 @@
 module JobsHelper
   def save_job(job, job_params)
-    if job_params["category_id"] == ""
-      no_category_error
-    elsif job.save
+    return no_category_error if job_params["category_id"] == ""
+    if job.save
       flash[:success] = "You created #{job.title} at #{job.company.name}"
       redirect_to company_job_path(job.company, job)
     else
@@ -11,9 +10,8 @@ module JobsHelper
   end
 
   def update_job(job, job_params)
-    if job_params["category_id"] == ""
-      no_category_error
-    elsif job.update(job_params)
+    return no_category_error if job_params["category_id"] == ""
+    if job.update(job_params)
       flash[:success] = "#{job.title} was updated!"
       redirect_to company_job_path(job.company, job)
     else
