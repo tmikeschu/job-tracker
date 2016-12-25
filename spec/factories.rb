a187aa492d73fbd833dbfd4a60cdf821b0fd8c16
Mike Schutte <tmikeschutte@gmail.com>
2016-12-25 08:42:37 -0800

    Add user factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,24 @@
 require 'factory_girl_rails'
 
 FactoryGirl.define do
+  factory :user do
+    sequence :first_name do
+      Faker::Name.first_name
+    end
+
+    sequence :last_name do
+      Faker::Name.last_name
+    end
+
+    sequence :email do |n|
+      "#{n}#{Faker::Internet.email}"
+    end
+
+    sequence :password do
+      Faker::Internet.password(10, 50)
+    end
+  end
+  
   factory :contact do
     sequence :first_name do
       Faker::Name.first_name
