827a2bd202e6a00c9f4fc64d393bad13182f62db
Mike Schutte <tmikeschutte@gmail.com>
2016-12-25 08:43:09 -0800

    Add passing feature test for user creates account

---
+++ spec/features/users/user_creates_an_account_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.feature "User creates an account" do
+  scenario "they can create an account" do
+    visit root_path
+    expect(page).to_not have_content "let's get you hired"
+    click_on "Create an Account"
+    fill_in "user_first_name", with: "Mike"
+    fill_in "user_last_name", with: "Schutte"
+    fill_in "user_email", with: "t@mike.schutte"
+    fill_in "user_password", with: "password123"
+    click_on "Create Account"
+
+    expect(page).to have_content "Account created. Welcome!"
+  end
+end
