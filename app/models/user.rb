cc0db54b9410d2b8adadeacc196222a358bd2753
Mike Schutte <tmikeschutte@gmail.com>
2016-12-25 07:57:17 -0800

    Add passing model test for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
+
+  validates :first_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: {in: 10..50}, on: :create
+  
   has_many :jobs
   has_many :contacts
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe User, type: :model do
     it {should have_many(:jobs)}
     it {should have_many(:contacts)}
   end
+
+  describe "validations" do
+    it {should validate_presence_of(:first_name)}
+
+    it {should validate_presence_of(:email)}
+    it {should validate_uniqueness_of(:email)}
+
+    it {should validate_presence_of(:password)}
+    it {should validate_length_of(:password).is_at_least(10).is_at_most(50)}
+    end
 end
