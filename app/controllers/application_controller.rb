7f029eeb2eacb661f219d06d33598bd4ef7f945a
Mike Schutte <tmikeschutte@gmail.com>
2016-12-26 10:39:15 -0800

    Move require login to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
   protect_from_forgery with: :exception
 
   helper_method :current_user
@@ -6,4 +8,9 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  private
+    def require_login
+      redirect_to login_path unless current_user
+    end
 end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,12 +1,4 @@
 class DashboardController < ApplicationController
-  before_action :require_login
-
   def show
-    # byebug
   end
-
-  private
-    def require_login
-      redirect_to login_path unless current_user
-    end
 end
