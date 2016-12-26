dcadd435a8764a271138c227bd31adcf7dae2c04
Mike Schutte <tmikeschutte@gmail.com>
2016-12-26 08:42:33 -0800

    Change seeds for test db

--- Gemfile
+++ Gemfile
@@ -32,8 +32,8 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
-  gem 'faker'
-  gem 'factory_girl_rails', "~> 4.0"
+gem 'faker'
+gem 'factory_girl_rails', "~> 4.0"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,30 +1,37 @@
+require 'factory_girl_rails'
+
 Company.destroy_all
 Job.destroy_all
 Category.destroy_all
 Contact.destroy_all
+User.destroy_all
 
-COMPANIES = ["ESPN", "Aetna", "United Airlines", "Denver Public Schools", "Shopify", "Starbucks", "Pivotal Labs", "Captain U"]
-JOBS = ["Engineering", "Development", "Dev Ops", "Quality Assurance", "Teacher", "Product Manager", "Consultant", "Community Manager"]
-CITIES = ["Seattle", "Denver", "Portland", "Indianapolis", "Madison", "Orlando", "San Diego", "Austin", "Las Vegas", "Little Rock", "Boise", "Eugene", "Oakland"]
-CATEGORIES = ["Web Development", "DevOps", "Startup", "Backend", "Full Stack"]
+# COMPANIES = ["ESPN", "Aetna", "United Airlines", "Denver Public Schools", "Shopify", "Starbucks", "Pivotal Labs", "Captain U"]
+# JOBS = ["Engineering", "Development", "Dev Ops", "Quality Assurance", "Teacher", "Product Manager", "Consultant", "Community Manager"]
+# CITIES = ["Seattle", "Denver", "Portland", "Indianapolis", "Madison", "Orlando", "San Diego", "Austin", "Las Vegas", "Little Rock", "Boise", "Eugene", "Oakland"]
+# CATEGORIES = ["Web Development", "DevOps", "Startup", "Backend", "Full Stack"]
 
-CATEGORIES.each do |title|
-  Category.create(title: title)
+20.times do 
+  FactoryGirl.create(:full_user)
 end
 
-20.times do
-  Contact.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, position: Faker::Name.title, email: Faker::Internet.email)
-end
+# CATEGORIES.each do |title|
+#   Category.create(title: title)
+# end
 
-i = 0
-COMPANIES.each do |name|
-  company = Company.create!(name: name)
-  company.contacts = Contact.offset(i).limit(2)
-  i += 2
-  company.save
-  puts "Created #{company.name}"
-  10.times do |num|
-    company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, category_id: (rand(CATEGORIES.count) + 1))
-    puts "  Created #{company.jobs[num].title}"
-  end
-end
+# 20.times do
+#   Contact.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, position: Faker::Name.title, email: Faker::Internet.email)
+# end
+
+# i = 0
+# COMPANIES.each do |name|
+#   company = Company.create!(name: name)
+#   company.contacts = Contact.offset(i).limit(2)
+#   i += 2
+#   company.save
+#   puts "Created #{company.name}"
+#   10.times do |num|
+#     company.jobs.create!(title: JOBS.sample, description: "What a great position!", level_of_interest: num + rand(100), city: CITIES.sample, category_id: (rand(CATEGORIES.count) + 1))
+#     puts "  Created #{company.jobs[num].title}"
+#   end
+# end
