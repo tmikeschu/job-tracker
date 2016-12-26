09dc753aae3e7a7ee2aa9683a343738248991ff8
Mike Schutte <tmikeschutte@gmail.com>
2016-12-26 13:34:05 -0800

    Fix user deletes category feature test

--- spec/features/categories/user_deletes_a_category_spec.rb
+++ spec/features/categories/user_deletes_a_category_spec.rb
@@ -4,8 +4,7 @@ RSpec.feature "User deletes a category" do
   before do
     @user = create(:full_user)
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
-    categories = Category.joins(:jobs).where("jobs.user_id = ?", @user.id).order(:title)
-    @category  = categories.first
+    @category  = @user.categories.order(:title).first
     visit categories_path
   end
 
