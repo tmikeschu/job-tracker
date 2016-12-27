05ea8c1ad95ad993c7a650e3e5dab11110915f3d
Mike Schutte <tmikeschutte@gmail.com>
2016-12-27 14:45:24 -0700

    Add login to delete company feature test

--- spec/features/companies/user_deletes_a_company_spec.rb
+++ spec/features/companies/user_deletes_a_company_spec.rb
@@ -2,13 +2,15 @@ require 'rails_helper'
 
 RSpec.feature "User deletes existing company" do
   scenario "a user can delete a company" do
-    company = Company.create(name: "ESPN")
+    @user = create(:full_user)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    company = @user.companies.first
     visit companies_path
 
     within(".company_#{company.id}") do
       click_link "Delete"
     end
 
-    expect(page).to have_content("ESPN was successfully deleted!")
+    expect(page).to have_content("#{company.name} was successfully deleted!")
   end
 end
