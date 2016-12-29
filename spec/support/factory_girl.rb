4b07ef72bb55983a77f31dc96c31b6fee569e495
Mike Schutte <tmikeschutte@gmail.com>
2016-12-29 11:57:22 -0700

    Add seed load to RSpec config

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -4,12 +4,12 @@ RSpec.configure do |c|
   c.include FactoryGirl::Syntax::Methods
 
   c.before(:all) do
-    begin
-      DatabaseCleaner.clean
-      FactoryGirl.lint
-    ensure
-      DatabaseCleaner.clean
-    end
+    DatabaseCleaner.clean
+    FactoryGirl.lint
+  end
+
+  c.before(:each) do
+    Rails.application.load_seed
   end
 
   c.after(:each) do
