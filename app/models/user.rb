2d491331f1a482ab0b2485a1deefaed11137ecaf
Mike Schutte <tmikeschutte@gmail.com>
2016-12-29 12:20:21 -0700

    Add dependent destroy to user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: {in: 10..50}, on: :create
   
-  has_many :jobs
-  has_many :contacts
-  has_many :companies
-  has_many :categories
+  has_many :jobs, dependent: :destroy
+  has_many :contacts, dependent: :destroy
+  has_many :companies, dependent: :destroy
+  has_many :categories, dependent: :destroy
 end
