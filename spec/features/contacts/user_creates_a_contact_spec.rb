c4da3261c5ca59eef54651f9d8da6eaae1ae205d
Mike Schutte <tmikeschutte@gmail.com>
2016-12-29 11:58:00 -0700

    Refactor with db user

--- spec/features/contacts/user_creates_a_contact_spec.rb
+++ spec/features/contacts/user_creates_a_contact_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "User creates a contact" do
   before do
-    @user = create(:full_user)
+    @user = User.first
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     @company = @user.companies.first
     @first_name, @last_name, @position, @email = %w(Sam Gerk developer samgerk@email.com)
