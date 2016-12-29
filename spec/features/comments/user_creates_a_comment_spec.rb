cd7b85b4fb8181312a9117e2bb2c668feb19e847
Mike Schutte <tmikeschutte@gmail.com>
2016-12-29 11:56:59 -0700

    Refactor with db user

--- spec/features/comments/user_creates_a_comment_spec.rb
+++ spec/features/comments/user_creates_a_comment_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature "User creates a comment" do
   before do
-    @user = create(:full_user)
+    @user = User.first
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     @company = @user.companies.first
     @jobs    = @company.jobs
